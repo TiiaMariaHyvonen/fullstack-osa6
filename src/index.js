1b62773657ca5b0df35b4802580c4082ed0e0187
TiiaMariaHyvonen <tiiamariahyvonen@outlook.com>
2023-02-18 16:41:04 +0200

    6.2

--- src/index.js
+++ src/index.js
@@ -9,9 +9,36 @@ const App = () => {
     return (
       <div>
         <div>
-          {store.getState().good}
-          {store.getState().ok}
-          {store.getState().bad}
+        </div>
+        <button 
+          onClick={e => store.dispatch({ type: 'GOOD' })}
+        >
+          good
+        </button>
+        <button
+          onClick={e => store.dispatch({ type: 'OK' })}
+        >
+          ok
+        </button>
+        <button 
+          onClick={e => store.dispatch({ type: 'BAD' })}
+        >
+          bad
+        </button>
+        <button 
+          onClick={e => store.dispatch({ type: 'ZERO' })}
+        >
+          reset stats
+        </button>
+
+        <div>
+          good {store.getState().good}
+        </div>
+        <div>
+          ok {store.getState().ok}
+        </div>
+        <div>
+          bad {store.getState().bad}
         </div>
       </div>
     )
